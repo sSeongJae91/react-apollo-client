d311e015397bc520d7ca3d3ac5f79d8d549ef783
sSeongjae91 <ben91429@gmail.com>
2021-02-22 22:40:52 +0900

    login & auth

--- src/compoenents/MenuBar.js
+++ src/compoenents/MenuBar.js
@@ -1,8 +1,11 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useContext } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+import { AuthContext } from '../context/auth';
+
 function MenuBar() {
+  const { user, logout } = useContext(AuthContext);
   const pathname = window.location.pathname;
   
   const path = pathname === '/' ? 'home' : pathname.substr(1)
@@ -10,33 +13,49 @@ function MenuBar() {
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
   
-    return (
-        <Menu pointing secondary size="massive" color="teal">
-          <Menu.Item
-            name='home'
-            active={activeItem === 'home'}
-            onClick={handleItemClick}
-            as={Link}
-            to="/"
-          />
-          <Menu.Menu position='right'>
-            <Menu.Item
-              name='login'
-              active={activeItem === 'login'}
-              onClick={handleItemClick}
-              as={Link}
-              to="/login"
-            />
-            <Menu.Item
-              name='register'
-              active={activeItem === 'register'}
-              onClick={handleItemClick}
-              as={Link}
-              to="/register"
-            />
-          </Menu.Menu>
-        </Menu>
-    )
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="teal">
+    <Menu.Item
+      name={user.username}
+      active
+      as={Link}
+      to="/"
+    />
+    <Menu.Menu position='right'>
+      <Menu.Item
+        name='logout'
+        onClick={logout}
+      />
+    </Menu.Menu>
+  </Menu>
+  ):(
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item
+        name='home'
+        active={activeItem === 'home'}
+        onClick={handleItemClick}
+        as={Link}
+        to="/"
+      />
+      <Menu.Menu position='right'>
+        <Menu.Item
+          name='login'
+          active={activeItem === 'login'}
+          onClick={handleItemClick}
+          as={Link}
+          to="/login"
+        />
+        <Menu.Item
+          name='register'
+          active={activeItem === 'register'}
+          onClick={handleItemClick}
+          as={Link}
+          to="/register"
+        />
+      </Menu.Menu>
+    </Menu>
+  )
+    return menuBar;
 }
 
 export default MenuBar;
